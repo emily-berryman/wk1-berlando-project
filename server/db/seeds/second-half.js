e89bed26ef301ca9cdb363b09eb6f520bf2e4221
Emily Berryman <emilyrberryman@gmail.com>
2021-01-19 10:12:40 +1300

    second-half

--- server/db/seeds/second-half.js
+++ server/db/seeds/second-half.js
@@ -7,43 +7,43 @@ exports.seed = function(knex) {
       return knex('second-half').insert([
         {
           id: 1, 
-          quote: 'rowValue1'
+          quote: 'you do not have to be a fire, you could be a water'
         },
         {
           id: 2, 
-          quote: 'rowValue2'
+          quote: 'be easy.'
         },
         {
           id: 3, 
-          quote: 'rowValue3'
+          quote: 'do not wait for someone else to come and speak for you'
         },
         {
           id: 4, 
-          quote: 'rowValue3'
+          quote: 'you are the sky.'
         },
         {
           id: 5, 
-          quote: 'rowValue3'
+          quote: 'sometimes your heart needs more time to accept what your mind already knows'
         },
         {
           id: 6, 
-          quote: 'rowValue3'
+          quote: 'things falling apart is a kind of testing and also a kind of healing.'
         },
         {
           id: 7, 
-          quote: 'rowValue3'
+          quote: "the truth is that things don't really get solved. They come together and they fall apart."
         },
         {
           id: 8, 
-          quote: 'rowValue3'
-        },
+          quote: "when we resist change, it's called suffering"
+        }
         {
           id: 9, 
-          quote: 'rowValue3'
+          quote: "I'm going to go out like a fucking meteor!"
         },
         {
           id: 10, 
-          quote: 'rowValue3'
+          quote: 'so that by the time we wake up'
         }
       ]);
     });
